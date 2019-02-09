1b188f8b3f821bb34912045bd0715f5681bc0485
rick venn <rv@lap.chop>
2019-02-09 16:08:49 +0000

    fiddle with faust compiler options

--- build_so.sh
+++ build_so.sh
@@ -1,5 +1,5 @@
-# cp faustglue.h $1.c
-# faust -lang c $1.dsp | sed -e s/"min("/"fmin("/g -e s/"max("/"fmax("/g>> $1.c
+cp faustglue.h $1.c
+faust -lang c -vec -vs 256 $1.dsp | sed -e s/"min("/"fmin("/g -e s/"max("/"fmax("/g>> $1.c
 gcc -O3 -c -fPIC $1.c -o $1.o
 gcc $1.o -shared -o $1.so
 
