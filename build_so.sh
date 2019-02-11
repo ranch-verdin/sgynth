eff0356ecce6ebd12a7f8d98cb65f94ceeeb6697
rick venn <rv@lap.chop>
2019-02-11 22:00:39 +0000

    make build script smarter

--- build_so.sh
+++ build_so.sh
@@ -1,13 +1,12 @@
-for faustfile in snarf drumbum hatz clack;
-do
-    # faust -lang c  -vec -vs 256
-    faust
-    have_faust = $?
-    if [ $OUT -eq 0 ];then
+faust --help &>/dev/null
+have_faust=$?
+if [ $have_faust -eq 0 ]; then
+    for faustfile in snarf drumbum hatz clack; do
+	# faust -lang c  -vec -vs 256
 	cp faustglue.h $faustfile.c
 	faust -lang c $faustfile.dsp | sed -e s/"min("/"fmin("/g -e s/"max("/"fmax("/g>> $faustfile.c
-    fi
-done
+    done
+fi
 gcc -O3 -Wall -c -fPIC $faustfile.c -o $faustfile.o
 gcc -O3 -Wall -c -fPIC -lmath engine_faustExample.c -o engine_faustExample.o
 gcc -O3 -Wall -c -fPIC faustglue.c -llo -ljack -ldl -o faustglue.o
