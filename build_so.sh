01c575e04c96eb541f767eb68f751f1dd84723ae
rick venn <rv@lap.chop>
2019-02-11 21:57:19 +0000

    check for faust, don't blat the c files

--- build_so.sh
+++ build_so.sh
@@ -1,11 +1,16 @@
-faustfile=drumbum
-
-
-cp faustglue.h $faustfile.c
-faust -lang c -vec -vs 256 $faustfile.dsp | sed -e s/"min("/"fmin("/g -e s/"max("/"fmax("/g>> $faustfile.c
-gcc -Wall -c -fPIC $faustfile.c -o $faustfile.o
-gcc -Wall -c -fPIC -lmath engine_faustExample.c -o engine_faustExample.o
-gcc -Wall -c -g -fPIC faustglue.c -llo -ljack -ldl -o faustglue.o
+for faustfile in snarf drumbum hatz clack;
+do
+    # faust -lang c  -vec -vs 256
+    faust
+    have_faust = $?
+    if [ $OUT -eq 0 ];then
+	cp faustglue.h $faustfile.c
+	faust -lang c $faustfile.dsp | sed -e s/"min("/"fmin("/g -e s/"max("/"fmax("/g>> $faustfile.c
+    fi
+done
+gcc -O3 -Wall -c -fPIC $faustfile.c -o $faustfile.o
+gcc -O3 -Wall -c -fPIC -lmath engine_faustExample.c -o engine_faustExample.o
+gcc -O3 -Wall -c -fPIC faustglue.c -llo -ljack -ldl -o faustglue.o
 gcc engine_faustExample.o faustglue.o $faustfile.o -shared -o $faustfile.so
 
 gcc -Wall -c -fPIC -lmath engine_example.c -o engine_example.o
