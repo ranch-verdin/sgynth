0ee8203efd3de20f89b3f70076b37b4247daacb5
Ranch Verdin <rick@bellatrix>
2019-03-02 19:26:10 +0000

    clean up docs, make build_all put .so in root directory

--- build_all.sh
+++ build_all.sh
@@ -1,4 +1,4 @@
-rm c_src/*.so c_src/*.o engine &>/dev/null
+rm `find . -name "*.o"` `find . -name "*.so"` engine &>/dev/null
 
 gcc -I include -O3 -Wall -c -fPIC -lmath c_src/engine_faustExample.c -o c_src/engine_faustExample.o
 gcc -I include -O3 -Wall -c -fPIC c_src/faustglue.c -llo -ljack -ldl -o c_src/faustglue.o
@@ -16,9 +16,10 @@ for faustfile in `ls faust_src/*.dsp | sed -e s/.dsp//g`; do
 	fi
     gcc -I include -O3 -Wall -c -fPIC $faustfile.c -o $faustfile.o
     gcc -I include c_src/engine_faustExample.o c_src/faustglue.o $faustfile.o -shared -o $faustfile.so
+    mv $faustfile.so ./
 done
 
 gcc -I include -Wall -c -fPIC -lmath engine_example/engine_example.c -o engine_example/engine_example.o
-gcc -shared engine_example/engine_example.o -o engine_example/engine_example.so
+gcc -shared engine_example/engine_example.o -o engine_example.so
 
 gcc -I include -Wall -g c_src/engine_main.c c_src/engineglue.c -llo -ljack -ldl -o engine
